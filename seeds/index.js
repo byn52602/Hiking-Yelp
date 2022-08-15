de208ec65f334d71d3da4606ff71a51aa5ce0c1f
Yani Bu <yanibu@Yanis-MacBook-Pro.local>
2022-08-15 11:11:04 -0400

    changed difficulty error msg

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,13 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const mongoose = require('mongoose')
 // const cities = require('./cities')
 const Hiking = require('../models/Hiking')
 // const axios = require("axios")
 const trails = require("./trail")
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/hiking';
+const dbUrl = process.env.DB_URL;
 // const dbUrl = 'mongodb://localhost:27017/hiking'; //27017 is default port number. using test database
 async function main() {
     await mongoose.connect(dbUrl);
